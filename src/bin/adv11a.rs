ce9d88b227d6f275bf068252cc5702ca903066d8
Jonas Paus <pausjonas@gmail.com>
2021-12-18 17:18:37 +0100

    Remove dead code from testing phase

--- src/bin/adv11a.rs
+++ src/bin/adv11a.rs
@@ -12,8 +12,6 @@ const VIZUALIZE: bool = false;
 
 fn main() {
     let input = include_str!("../../resources/input11.txt");
-    // let input = "5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526";
-    // let input = "11111\n19991\n19191\n19991\n11111";
 
     let lines = input.lines().count();
 
